a42c708e0a7786ced59b7c7207cb5cfc07d950ff
Crol55 <carlosorantes77@gmail.com>
2020-09-01 23:42:35 -0600

    prueba definitiva

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample in nodito!\n');
+  res.send('Hello Node.js Sample in n!\n');
 });
 
 var port = process.env.PORT||PORT;
